c50404ca5646700c6aa23b3fcdd43524773117b6
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-09-30 11:17:43 +0200

    added register route

---
+++ src/routes/user.route.js
@@ -0,0 +1,10 @@
+import express, {Router} from "express";
+import { registerUser } from "../controllers/user.controller.js";
+
+
+const router = Router();
+
+router.route("/register").post(registerUser)
+
+export default router
+
