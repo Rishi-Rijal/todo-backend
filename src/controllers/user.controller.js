5830c9c897fc68c09701c01ad04b27cda5ce54a3
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-09-30 17:01:33 +0200

    fixed promise bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,18 +1,17 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/ApiResponse";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import User from "../models/user.model.js";
 
 
 const generateAccessAndRefreshToken = async (userId)=>{
     try {
-        const user = User.findById(userId);
-        const accessToken = user.generateAccessToken();
-        const refreshToken = user.generateRefreshToken();
+        const user = await User.findById(userId);
+        const accessToken = await user.generateAccessToken();
+        const refreshToken = await user.generateRefreshToken();
         user.refreshToken = refreshToken
 
         await user.save({ validateBeforeSave: false });
-
         return { accessToken, refreshToken }
     } catch (error) {
         throw new ApiError(400, "Something went wrong while generating access and refresh token", error)
@@ -38,14 +37,14 @@ const registerUser = asyncHandler(async (req, res) => {
 
     //TODO check if both email and password are valid or not
 
-    const user = User.create({
+    const user = await User.create({
         name: name,
         username: name,
         password: password,
         email: email
     })
 
-    const createdUser = User.findById(user._id).select("-password -refreshToken")
+    const createdUser = await User.findById(user._id).select("-password -refreshToken")
     if (!createdUser) {
         throw new ApiError(500, "Soomething went wrong while saving the data")
     }
@@ -60,26 +59,26 @@ const loginUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "both fields are required")
     }
 
-    const user = User.findOne({ email: email }).select("-password -refreshToken")
+    const user = await User.findOne({ email: email }).select("-refreshToken")
 
     if (!user) {
         throw new ApiError(400, "user with email does not exist")
     }
 
-    const isValidPass = user.isPasswordCorrect(password);
+    const isValidPass = await user.isPasswordCorrect(password);
 
     if (!isValidPass) {
         throw new ApiError(400, "invalid password")
     }
 
-    const { accessToken, refreshToken } = generateAccessAndRefreshToken(user._id);
-
-    const loggedInUser = User.findById(user._id).select("-password -refreshToken")
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id);
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
 
     const options = {
         httpOnly: true,
         secure: true
     }
+
 
     return res
         .status(200)
@@ -128,6 +127,7 @@ const getCurrentUser = asyncHandler(async(req, res)=>{
 export {
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    getCurrentUser
 
 }
